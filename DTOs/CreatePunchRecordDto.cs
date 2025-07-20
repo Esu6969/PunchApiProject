07272299dbf4ad7a081b82280235560ed57a4c06
Dhruvil Mayatra <189455718+Esu6969@users.noreply.github.com>
2025-07-20 19:45:19 +0530

    Delete DTOs directory

--- DTOs/CreatePunchRecordDto.cs
+++
@@ -1,7 +0,0 @@
-namespace PunchApiProject.DTOs
-{
-    public class CreatePunchRecordDto
-    {
-        public string EmployeeName { get; set; } = string.Empty;
-    }
-}
--- DTOs/UpdatePunchOutDto.cs
+++
@@ -1,7 +0,0 @@
-namespace PunchApiProject.DTOs
-{
-    public class UpdatePunchOutDto
-    {
-        public int Id { get; set; }
-    }
-}
